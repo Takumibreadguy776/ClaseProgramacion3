e7657652173a5d2e2af1770f75ff91fb02e110d5
Takumibreadguy776 <ctello90@outlook.com>
2021-06-03 14:18:15 -0500

    Functional script added

--- Programacion 3/Assets/arreglo1.cs
+++ Programacion 3/Assets/arreglo1.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class arreglo1 : MonoBehaviour
 {
+
+    public GameObject[] Array;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class arreglo1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(Array[Random.Range(0,3)]);
+        }
     }
 }
