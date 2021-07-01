b288bd6f92296ed4d5a3856ec6221a6727c35b89
Takumibreadguy776 <ctello90@outlook.com>
2021-07-01 14:41:27 -0500

    enemy

--- Programacion 3/Assets/GAMEJAM/bala.cs
+++ Programacion 3/Assets/GAMEJAM/bala.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class bala : MonoBehaviour
 {
     public GameObject hitEffect;
-
+    //instansear efecto y destruirlo poco despues
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+       GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 3f);
+        Destroy(gameObject);
     }
 
 }
