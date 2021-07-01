e0edb650ddcc88112a2421947d23d8e940a3ba60
Takumibreadguy776 <ctello90@outlook.com>
2021-07-01 13:45:09 -0500

    sadvzcxfdsgdf

---
+++ Programacion 3/Assets/GAMEJAM/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float movespeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePOS;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("vertical");
+
+       mousePOS = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+
+}
