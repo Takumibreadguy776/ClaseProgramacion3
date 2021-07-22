25ee2476825f9d70845663f30bc164b65982527e
Dannycat <65506435+Ynnai@users.noreply.github.com>
2021-07-22 13:24:59 -0500

    no se que es esto

---
+++ Programacion 3/Assets/Danny/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "pokemon", menuName = "MyCardGame/pokemon", order = 1)]
+public class Pokemon: ScriptableObject
+{
+    public new string name;
+    public string tipo;
+    public int hp;
+    public int atk;
+    public int def;
+    public int vel;
+    public Sprite img;
+
+}
